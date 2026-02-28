5ed560e33f6e4fca65afc30b5e450ebd9506095e
dlion4 <dantemilimo@gmail.com>
2026-02-28 11:45:39 +0300

    updated the blogging and contact with whatsapp button

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category
+from apps.core._admin import _admin_site
 
-# Register your models here.
+
+@admin.register(Category, site=_admin_site)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "category")
+    search_fields = ("name",)
+
