87b3a2683721eca499832503da8435584f5fddd5
dlion4 <dantemilimo@gmail.com>
2026-03-02 05:40:31 +0300

    added the adds

--- static/js/project.js
+++ static/js/project.js
@@ -1,5 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "goey-toast/styles.css";
+import { gooeyToast, GooeyToaster } from "goey-toast";
 import "../sass/project.scss";
 import $ from "jquery";
+
+// Render toaster once
+const container = document.getElementById("toast-root");
+
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(<GooeyToaster position="top-left" />);
+}
 
 $(function () {
   const $toggleBtn = $("#toggleAccessPasskey");
@@ -9,11 +21,17 @@ $(function () {
     $toggleBtn.on("click", function () {
       const isPassword = $accessCodeInput.attr("type") === "password";
       $accessCodeInput.attr("type", isPassword ? "text" : "password");
+
       $(this).html(
         isPassword
           ? '<i class="bi bi-eye-slash"></i>'
-          : '<i class="bi bi-eye"></i>',
+          : '<i class="bi bi-eye"></i>'
       );
     });
   }
+
+  // Now this will work
+  gooeyToast.success("Changes saved", {
+    description: "Your changes have been saved and synced successfully.",
+  });
 });
